22a0b4bf474b686266775b75c78d0503846cc4c3
Sultan-Seytbekov <210103170@stu.sdu.edu.kz>
2024-04-12 00:11:39 +0500

    first commit

---
+++ src/main/java/kz/sultan/HomeWorkApp.java
@@ -0,0 +1,13 @@
+package kz.sultan;
+
+/**
+ * Hello world!
+ *
+ */
+public class HomeWorkApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
